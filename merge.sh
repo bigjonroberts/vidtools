de29b12ad8f8ff98dfd84b8f2f39750b73a5e0c8
Jon Roberts <bigjonroberts@gmail.com>
2018-07-30 00:16:56 -0500

    fix shebang and change file extension

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
-#/bin/bash
-find . -type f -name '*.MOV' -exec echo "file {}" >> files_to_process \;
+#!/bin/bash
+find . -type f -name '*.MTS' -exec echo "file {}" >> files_to_process \;
 ffmpeg -safe 0 -f concat -i files_to_process -vcodec copy -an merged.MP4
 
